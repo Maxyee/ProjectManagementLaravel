6375180c8ce6d06b1727799aa6be2ef16c8e580a
Maxyee <eyaminkhan00@gmail.com>
2018-04-15 01:17:19 +0600

    register and login section using laravel auth

--- projectmanagement/resources/views/welcome.blade.php
+++ projectmanagement/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Project Management System in Laravel
                 </div>
 
                 <div class="links">
